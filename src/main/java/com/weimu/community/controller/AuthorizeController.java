c93685a79d6001d494f4c393e6470742e6b5ea3b
qq1098155959 <1098155959@qq.com>
2019-07-21 14:21:03 +0800

    add check cookie

--- src/main/java/com/weimu/community/controller/AuthorizeController.java
+++ src/main/java/com/weimu/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package com.weimu.community.controller;
 
 import com.weimu.community.dto.AccessTokenDto;
 import com.weimu.community.dto.GithubUser;
+import com.weimu.community.mapper.UserMapper;
+import com.weimu.community.model.User;
 import com.weimu.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,8 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -25,10 +28,13 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name ="state") String state,
-                            HttpServletRequest request){
+                           HttpServletResponse response){
         AccessTokenDto accessTokenDto = new AccessTokenDto();
         accessTokenDto.setClient_id(clientId);
         accessTokenDto.setClient_secret(clentSecret);
@@ -36,10 +42,17 @@ public class AuthorizeController {
         accessTokenDto.setRedirect_uri(redirectUri);
         accessTokenDto.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDto);
-        GithubUser user = githubProvider.getUser(accessToken);
-        if(user != null){
-            //登录成功
-            request.getSession().setAttribute("user",user);
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if(githubUser != null){
+            User user = new User();
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            response.addCookie(new Cookie("token",token));
             return "redirect:/";
         }else {
             //登录失败,重新登录
---
+++ src/main/java/com/weimu/community/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.weimu.community.mapper;
+
+import com.weimu.community.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+    @Insert("insert into user (name,accountid,token,gmtcreate,gmtmodified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
+}
